a355688fddabb96182dc5195d45cecf6fa764c4d
Tomo Krajina <tkrajina@gmail.com>
2020-07-06 17:31:56 +0200

    Cleanup

--- gitutils/__init__.py
+++ gitutils/__init__.py
@@ -1,11 +1,1 @@
 # -*- coding: utf-8 -*-
-
-from . import git
-
-is_changed = git.is_changed
-execute_command = git.execute_command
-get_config_properties = git.get_config_properties
-assert_in_git_repository = git.assert_in_git_repository
-get_branches = git.get_branches
-execute_git = git.execute_git
-delete_branch = git.delete_branch
